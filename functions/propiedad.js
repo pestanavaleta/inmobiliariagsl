f352dc8abf59f6b8996bb97ef266b421f389b274
artesfox <102455249+artesfox@users.noreply.github.com>
2026-02-12 22:57:11 -0500

    Update propiedad.js

--- functions/propiedad.js
+++ functions/propiedad.js
@@ -462,6 +462,7 @@ export async function onRequest(context) {
                             <div class="item-detalle-fx"><span>Habitaciones:</span> <span>${getDato("HABITACIONES")}</span></div>
                             <div class="item-detalle-fx"><span>Baños:</span> <span>${getDato("BAÑOS")}</span></div>
                             <div class="item-detalle-fx"><span>Área construida:</span> <span>${getDato("ÁREA CONSTRUIDA")}</span></div>
+							 <div class="item-detalle-fx"><span>Área del lote:</span> <span>${getDato("ÁREA DEL LOTE")}</span></div>
                             <div class="item-detalle-fx"><span>País:</span> <span>${getDato("PÁIS")}</span></div>
                             <div class="item-detalle-fx"><span>Ciudad:</span> <span>${getDato("CIUDAD/UBICACIÓN")}</span></div>
                             <div class="item-detalle-fx"><span>Zona:</span> <span>${getDato("ZONA")}</span></div>
