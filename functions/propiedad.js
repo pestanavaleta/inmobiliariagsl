17f026d03fea1f8c77bbc35c2d422a21dc5feabc
artesfox <102455249+artesfox@users.noreply.github.com>
2026-02-15 19:12:13 -0500

    Update propiedad.js

--- functions/propiedad.js
+++ functions/propiedad.js
@@ -457,7 +457,7 @@ grid-template-columns: 1fr 1fr;
                 <ul>
                     
                     <li><a href="propiedades.html" class="item-activo">Propiedades</a></li>
-					<li><a href="propiedades.html#nosotros">Nosotros</a></li>
+					<li><a href="https://santo-grial-inmobiliaria-free.pages.dev/propiedades#nosotros">Nosotros</a></li>
                  	<li><a href="#" class="cta-boton"><i class="houzez-icon icon-messaging-whatsapp" aria-hidden="true" style="font-size: 20px;"></i>Contacto</a></li>
                     <!--Submenú <li class="has-submenu">
                         <a href="#" class="submenu-trigger">
