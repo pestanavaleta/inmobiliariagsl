bfdfe5dba9a8fc1c8a0108a182dcaf1d66d5263a
artesfox <102455249+artesfox@users.noreply.github.com>
2026-02-17 23:07:33 -0500

    Update index2.js

--- functions/index2.js
+++ functions/index2.js
@@ -241,13 +241,13 @@ function generarPlantilla(tarjetas, total, c) {
 
     <footer class="footer relleno-1">
         <div class="contenedor info">
-            <article>
+            <article class="info">
                 ${c.logo ? `<img src="${c.logo}" style="max-width:150px;">` : `<h2>${c.nombre}</h2>`}
                 <p style="margin-top:20px;">© 2026 ${c.nombre}</p>          
 				    <ul class="redes">
                     	<li>${c.fb ? `<a href="${c.fb}" style="color:#fff; margin-right:10px;"><i class="houzez-icon icon-social-media-facebook"></i></a>` : ''}</li>
-                    	<li>${c.ig ? `<a href="${c.ig}" style="color:#fff; margin-right:10px;"><i class="houzez-icon icon-x-logo-twitter-logo-2"></i></a>` : ''}</li>
-						<li>${c.x ? `<a href="${c.x}" style="color:#fff; margin-right:10px;"><i class="houzez-icon icon-social-instagram"></i></a>` : ''}</li>
+                    	<li>${c.ig ? `<a href="${c.ig}" style="color:#fff; margin-right:10px;"><i class="houzez-icon  icon-social-instagram"></i></a>` : ''}</li>
+						<li>${c.x ? `<a href="${c.x}" style="color:#fff; margin-right:10px;"><i class="houzez-icon icon-x-logo-twitter-logo-2"></i></a>` : ''}</li>
 					</ul>
                
             </article>
@@ -368,6 +368,7 @@ function generarPlantilla(tarjetas, total, c) {
 </body>
 </html>`;
 }
+
 
 
 
